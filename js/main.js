90ffaf5473d7700ca4da7e06f712a4fe2b7ae8b6
ayrton1907 <lopezayrtonemilio@gmail.com>
2018-09-29 23:25:00 +0200

    vinculacion

---
+++ js/main.js
@@ -0,0 +1,48 @@
+function crearArt(nombreArt,img){
+
+  var articulos = document.getElementById('articulos');
+  var articulo = document.createElement('article');
+  var titulo = document.createElement('h2');
+  var imagen = document.createElement('img');
+  var linea = document.createElement("hr");
+
+  titulo.innerText=nombreArt;
+  imagen.src=img;
+  imagen.setAttribute('width','100%');
+
+  articulo.appendChild(titulo);
+  articulo.appendChild(imagen);
+  articulos.appendChild(articulo);
+  articulos.appendChild(linea);
+}
+
+function accionBoton(){
+
+  var titulo = document.getElementById('titulo').value;
+  var imagen = document.getElementById('imagen').value;
+
+  crearArt(titulo,imagen);
+
+}
+
+
+
+/*
+  var parrafo = document.createElement('p'); // este es para crear un elemento de tipo p
+  var elemento = ...;
+  var parrafo = ...;
+  elemento.appendChild(parrafo); // y esto es para agregar un "parrafo" en elemento.
+
+  elemento.innerText = "mi texto"; //cambia el texto dentro del elemento por "mi texto";
+
+  ________________________________
+
+  Crear un elemento article.
+  Crear un elemento h1
+  crear un elemento img
+  crear un elemento p
+
+  meter el h1,img,p dentro de article
+
+  meter article, dentro de articulos
+*/

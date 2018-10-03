de87100a8e3717bf793941cd643372a3d33b8c41
ayrton1907 <lopezayrtonemilio@gmail.com>
2018-10-03 22:12:10 +0200

    Crear articulos(de verdad)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-function crearArt(nombreArt,img){
+var listaArt = [];
+
+function guardarArt(listaArt){
+  var lista = JSON.stringify(listaArt);
+  localStorage.setItem("listaDeArticulos",lista);
+}
+
+function addArticulo(listaArt,nomArt,imgArt){
+  var art = {
+    titulo: nomArt,
+    imagen: imgArt
+  }
+  listaArt.push(art);
+  guardarArt(listaArt);
+}
+
+function cargarArt(){
+  var datos = localStorage.getItem("listaDeArticulos");
+  listaArt =  JSON.parse(datos);
+}
+
+function mostrarArt(nombreArt,img){
 
   var articulos = document.getElementById('articulos');
   var articulo = document.createElement('article');
@@ -15,6 +36,14 @@ function crearArt(nombreArt,img){
   articulos.appendChild(articulo);
   articulos.appendChild(linea);
 }
+function mostrarListaDeArt(listaArt) {
+  var i=0;
+  while (i < listaArt.length) {
+    mostrarArt(listaArt[i].titulo,listaArt[i].imagen);
+    i++;
+  }
+
+}
 
 function accionBoton(){
 
@@ -24,14 +53,11 @@ function accionBoton(){
   var reader  = new FileReader();
   reader.onloadend = function () {
     var imagenUrl = reader.result;
-    crearArt(titulo,imagenUrl);
+    mostrarArt(titulo,imagenUrl);
+    addArticulo(listaArt,titulo,imagenUrl);
   }
-
   reader.readAsDataURL(imagen);
-
 }
-
-
 
 /*
   var parrafo = document.createElement('p'); // este es para crear un elemento de tipo p
@@ -51,4 +77,13 @@ function accionBoton(){
   meter el h1,img,p dentro de article
 
   meter article, dentro de articulos
+  ______________________________________
+
+
+
+
+
+
+
+
 */
